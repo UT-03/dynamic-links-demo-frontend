ae5205579016b13d28a3f9f7150b059c20ff8375
Utkarsh Tiwari <79445012+UT-03@users.noreply.github.com>
2022-07-19 16:03:57 +0530

    feat: add global constants

---
+++ src/constants/colors.js
@@ -0,0 +1,15 @@
+const colors = {
+    primary100: '#e0cffc',
+    primary200: '#c29ffa',
+    primary400: '#8540f5',
+    primary500: '#6610f2',
+    primary700: '#3d0a91',
+    primary800: '#290661',
+    primary900: '#140330',
+    gray400: '#ced4da',
+    gray600: '#6c757d',
+    error50: '#f8d7da',
+    error500: '#dc3545'
+}
+
+export default colors;
---
+++ src/constants/globalVariables.js
@@ -0,0 +1,6 @@
+const globalVariables = {
+    backendHost: 'http://192.168.147.57:5000/api',
+    backendURL: 'http://192.168.147.57:5000',
+}
+
+export default globalVariables;
