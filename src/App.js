7353504994ee2c5752643920bfa1908fe64e8573
Utkarsh Tiwari <79445012+UT-03@users.noreply.github.com>
2022-07-19 09:41:47 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import { Text, View } from 'react-native';
+import { LogBox } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>App.js</Text>
+    </View>
+  );
+};
+
+export default App;
